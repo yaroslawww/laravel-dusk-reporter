967262f06a7336a3635c9879072f9acc21701a28
Yaro WebDev <yg@think.studio>
2021-04-30 10:47:41 +0300

    Improve readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Report generator for Laravel Dusk
 
+Report will be saved in `.md` files.
+
 ## Installation
 
 You can install the package via composer:
@@ -7,6 +9,109 @@ You can install the package via composer:
 ```bash
 composer require --dev laravel/dusk
 composer require --dev yaroslawww/laravel-dusk-reporter
+```
+
+## Usage
+
+1. Use trait `WithDuskReport` in your dusk test case. This is optional, but it will be easy to rename the file.
+
+```injectablephp
+//...
+use ThinkOne\LaravelDuskReporter\WithDuskReport;
+
+abstract class DuskTestCase extends BaseTestCase
+{
+    use WithDuskReport;
+    
+    //...
+    
+}
+```
+
+2. You can override default configuration
+
+```injectablephp
+//...
+use ThinkOne\LaravelDuskReporter\Reporter;
+use ThinkOne\LaravelDuskReporter\WithDuskReport;
+
+abstract class DuskTestCase extends BaseTestCase
+{
+    use WithDuskReport;
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // Change store report folder, by default package use "storage_path('laravel-dusk-reporter')"
+        Reporter::$storeBuildAt = app_path('reports/dusk-report');
+        
+        // Change store screenshots folder, by default package use field "$storeBuildAt"
+        Reporter::$storeScreenshotAt = app_path('reports/dusk-screenshots');
+        
+        // By default package save link in `.md` files as relative path, you can change it
+        Reporter::$screenshotRelativePath = false;
+         
+        // Change element to fit content (by default package use "body" tag)
+        Reporter::$getBodyElementCallback = function ($browser) {
+            return $browser->driver->findElement(WebDriverBy::id('root'));
+        };
+    }
+    
+    //...
+    
+}
+```
+
+3. Create report
+
+```injectablephp
+/** @test */
+public function open_not_logged_user() {
+    $REPORT = $this->newDuskReportFile()
+                   ->h1( 'Home marketing page' )
+                   ->br()->h2( 'Initial data' )->br()
+                   ->raw( "Open page by not logged user" )->br()
+                   ->h2( 'Result' )->br();
+
+    $this->browse( function ( Browser $browser ) use ( $REPORT ) {
+        $browser->visit( new HomePage() )
+                // ...
+                ->assertPresent( '@header' )
+                ->assertPresent( '@footer' );
+
+        $REPORT->screenshot( $browser, 1, ReportScreenshot::RESIZE_COMBINE );
+    } );
+    
+    $REPORT->br()->h3('Conclusion')->br()->raw('Test passed.');
+}
+```
+
+By default you will see report directories tree like this:
+
+```
+- storage
+-- laravel-dusk-reporter
+--- MarketingHomePageTest
+---- open_not_logged_user.md
+---- open_not_logged_user_1.png
+```
+
+File `open_not_logged_user.md` will contain this data:
+
+```
+# Home marketing page
+
+## Initial data
+
+Open page by not logged user
+## Result
+
+![MarketingHomePageTest/open_not_logged_user_1](open_not_logged_user_1.png)
+
+### Conclusion
+
+Test passed.
 ```
 
 ## Credits
--- src/Generation/ReportScreenshot.php
+++ src/Generation/ReportScreenshot.php
@@ -8,8 +8,12 @@ use Imagick;
 use Laravel\Dusk\Browser;
 use ThinkOne\LaravelDuskReporter\Reporter;
 
-class ReportScreenshot
-{
+class ReportScreenshot {
+
+    const RESIZE_FIT = 'fit';
+
+    const RESIZE_COMBINE = 'combine';
+
     protected Reporter $reporter;
 
     /**
@@ -17,14 +21,14 @@ class ReportScreenshot
      *
      * @param Reporter $reporter
      */
-    public function __construct(Reporter $reporter)
-    {
+    public function __construct( Reporter $reporter ) {
         $this->reporter = $reporter;
     }
 
 
     /**
      * Crete screenshot
+     *
      * @param Browser $browser
      * @param string $filename
      * @param string $resize
@@ -32,24 +36,23 @@ class ReportScreenshot
      * @return string
      * @throws \ImagickException
      */
-    public function make(Browser $browser, string $filename, string $resize = 'fit'): string
-    {
+    public function make( Browser $browser, string $filename, string $resize = 'fit' ): string {
         $defaultStoreScreenshotsAt = $browser::$storeScreenshotsAt;
 
         $browser::$storeScreenshotsAt = $this->reporter->storeScreenshotAt();
 
-        if ($resize == 'combine') {
-            $this->reportScreenCombined($browser, $filename);
+        if ( $resize == static::RESIZE_COMBINE ) {
+            $this->reportScreenCombined( $browser, $filename );
         } else {
             $defaultSize = $browser->driver->manage()->window()->getSize();
-            if ($resize == 'fit') {
-                $browser = $this->fitContent($browser);
+            if ( $resize == static::RESIZE_FIT ) {
+                $browser = $this->fitContent( $browser );
             }
 
-            $browser->screenshot($filename);
+            $browser->screenshot( $filename );
 
-            if ($resize == 'fit' && $defaultSize) {
-                $browser->resize($defaultSize->getWidth(), $defaultSize->getHeight());
+            if ( $resize == static::RESIZE_FIT && $defaultSize ) {
+                $browser->resize( $defaultSize->getWidth(), $defaultSize->getHeight() );
             }
         }
 
@@ -59,26 +62,24 @@ class ReportScreenshot
     }
 
 
-    public function fitContent(Browser $browser)
-    {
+    public function fitContent( Browser $browser ) {
         try {
-            $body = $this->getBodyElement($browser);
+            $body        = $this->getBodyElement( $browser );
             $currentSize = $body->getSize();
-            $browser->resize($currentSize->getWidth(), $currentSize->getHeight());
-        } catch (\Exception $e) {
+            $browser->resize( $currentSize->getWidth(), $currentSize->getHeight() );
+        } catch ( \Exception $e ) {
             $browser->fitContent();
         }
 
         return $browser;
     }
 
-    protected function getBodyElement(Browser $browser): \Facebook\WebDriver\Remote\RemoteWebElement
-    {
-        if (is_callable($this->reporter::$getBodyElementCallback)) {
-            return call_user_func($this->reporter::$getBodyElementCallback, $browser);
+    protected function getBodyElement( Browser $browser ): \Facebook\WebDriver\Remote\RemoteWebElement {
+        if ( is_callable( $this->reporter::$getBodyElementCallback ) ) {
+            return call_user_func( $this->reporter::$getBodyElementCallback, $browser );
         }
 
-        return $browser->driver->findElement(WebDriverBy::tagName('body'));
+        return $browser->driver->findElement( WebDriverBy::tagName( 'body' ) );
     }
 
     /**
@@ -88,41 +89,40 @@ class ReportScreenshot
      * @return Browser
      * @throws \ImagickException
      */
-    protected function reportScreenCombined(Browser $browser, string $filename)
-    {
-        $windowSize = $browser->driver->manage()->window()->getSize();
+    protected function reportScreenCombined( Browser $browser, string $filename ) {
+        $windowSize   = $browser->driver->manage()->window()->getSize();
         $windowHeight = $windowSize->getHeight();
-        $body = $this->getBodyElement($browser);
-        $fullHeight = $body->getSize()->getHeight();
-        $counter = 0;
-        $offset = 0;
-        $files = [];
-        while ($offset < $fullHeight) {
-            $browser->driver->executeScript('window.scrollTo(0, ' . $offset . ');');
-            if ($windowHeight > ($needCapture = ($fullHeight - $offset))) {
-                $browser->resize($windowSize->getWidth(), $needCapture);
-                $browser->driver->executeScript('window.scrollTo(0, document.body.scrollHeight);');
+        $body         = $this->getBodyElement( $browser );
+        $fullHeight   = $body->getSize()->getHeight();
+        $counter      = 0;
+        $offset       = 0;
+        $files        = [];
+        while ( $offset < $fullHeight ) {
+            $browser->driver->executeScript( 'window.scrollTo(0, ' . $offset . ');' );
+            if ( $windowHeight > ( $needCapture = ( $fullHeight - $offset ) ) ) {
+                $browser->resize( $windowSize->getWidth(), $needCapture );
+                $browser->driver->executeScript( 'window.scrollTo(0, document.body.scrollHeight);' );
             }
-            $browser->screenshot($screenName = "{$filename}_{$counter}");
-            $files[] = $filePath = sprintf('%s/%s.png', rtrim($browser::$storeScreenshotsAt, '/'), $screenName);
-            $counter++;
+            $browser->screenshot( $screenName = "{$filename}_{$counter}" );
+            $files[] = $filePath = sprintf( '%s/%s.png', rtrim( $browser::$storeScreenshotsAt, '/' ), $screenName );
+            $counter ++;
             $offset += $windowHeight;
         }
-        $browser->resize($windowSize->getWidth(), $windowSize->getHeight());
-        $browser->driver->executeScript('window.scrollTo(0, 0);');
+        $browser->resize( $windowSize->getWidth(), $windowSize->getHeight() );
+        $browser->driver->executeScript( 'window.scrollTo(0, 0);' );
 
         $im = new Imagick();
-        foreach ($files as $file) {
-            $im->readImage($file);
-            unlink($file);
+        foreach ( $files as $file ) {
+            $im->readImage( $file );
+            unlink( $file );
         }
         /* Append the images into one */
         $im->resetIterator();
-        $combined = $im->appendImages(true);
+        $combined = $im->appendImages( true );
 
         /* Output the image */
-        $combined->setImageFormat('png');
-        $combined->writeImage(sprintf('%s/%s.png', rtrim($browser::$storeScreenshotsAt, '/'), $filename));
+        $combined->setImageFormat( 'png' );
+        $combined->writeImage( sprintf( '%s/%s.png', rtrim( $browser::$storeScreenshotsAt, '/' ), $filename ) );
 
         return $browser;
     }
